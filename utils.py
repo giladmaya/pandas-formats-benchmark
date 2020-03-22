6f76c241131745153a2649569f9a147cb77eeb2a
giladmaya <ms.maya.gilad@gmail.com>
2020-03-22 15:57:10 +0200

    Added compression to benchmark

--- utils.py
+++ utils.py
@@ -1,12 +1,20 @@
 import gc
+import gzip
+import shutil
+from collections import defaultdict
+from functools import partial
 from multiprocessing import Process, Event, Value
 import os
 import time
+from tempfile import NamedTemporaryFile, TemporaryFile
 from timeit import default_timer
 
+import feather
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import psutil
+from pyarrow.lib import ArrowInvalid
 
 
 class Timer:
@@ -126,3 +134,145 @@ class NotebookStyle(VisualStyle):
             'ytick.labelsize': 14,
             'font.size': 16
         })
+
+
+def generate_dataset(n_rows, num_count, cat_count, max_nan=0.1, max_cat_size=100):
+    """Randomly generate datasets with numerical and categorical features.
+
+    The numerical features are taken from the normal distribution X ~ N(0, 1).
+    The categorical features are generated as random uuid4 strings with
+    cardinality C where 2 <= C <= max_cat_size.
+
+    Also, a max_nan proportion of both numerical and categorical features is replaces
+    with NaN values.
+    """
+    dataset, types = {}, {}
+
+    def generate_categories():
+        from uuid import uuid4
+        category_size = np.random.randint(2, max_cat_size)
+        return [str(uuid4()) for _ in range(category_size)]
+
+    for col in range(num_count):
+        name = f'n{col}'
+        values = np.random.normal(0, 1, n_rows)
+        nan_cnt = np.random.randint(1, int(max_nan * n_rows))
+        index = np.random.choice(n_rows, nan_cnt, replace=False)
+        values[index] = np.nan
+        dataset[name] = values
+        types[name] = 'float32'
+
+    for col in range(cat_count):
+        name = f'c{col}'
+        cats = generate_categories()
+        values = np.array(np.random.choice(cats, n_rows, replace=True), dtype=object)
+        nan_cnt = np.random.randint(1, int(max_nan * n_rows))
+        index = np.random.choice(n_rows, nan_cnt, replace=False)
+        values[index] = np.nan
+        dataset[name] = values
+        types[name] = 'object'
+
+    return pd.DataFrame(dataset), types
+
+
+def size_of(filename, unit=1024**2):
+    return round(os.stat(filename).st_size / unit, 2)
+
+
+def save_feather(df, fname, compress=False, compresslevel=7):
+    if compress is False:
+        df.to_feather(fname)
+    else:
+        tmpf = NamedTemporaryFile()
+        df.to_feather(tmpf.name)
+        with open(tmpf.name, mode='rb') as f_in:
+            with gzip.open(fname, compresslevel=compresslevel, mode='wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        tmpf.close()
+
+
+def read_feather(source, columns=None, use_threads=True):
+    try:
+        return feather.read_dataframe(source, columns, use_threads)
+    except ArrowInvalid:
+        fin = open(source, 'rb')
+        tmpf = TemporaryFile('w+b')
+        with gzip.GzipFile(fileobj=fin, mode='rb') as fgz:
+            data = fgz.read()
+            tmpf.write(data)
+        tmpf.seek(0)
+        return feather.read_dataframe(tmpf, columns, use_threads)
+
+
+def get_save_load(df, fmt):
+    if fmt == 'feather':
+        return partial(save_feather, df), read_feather
+    return getattr(df, f'to_{fmt}'), getattr(pd, f'read_{fmt}')
+
+
+def benchmark(list_of_formats, data_size=1_000_000, n_num=15, n_cat=15, n_rounds=20,
+              as_category=False):
+    """Runs dataset saving/loading benchamrk using formts from the list_of_formats.
+
+    Each round a new random dataset is generated with data_size observations.
+    The measurements for each of the rounds are concatenated together and returned
+    as a single data frame.
+
+    Parameters:
+        list_of_formats: A list of tuples in the format (<format_name>, [<params_dict>]).
+            The <format_name> should be one of the pandas supported formats.
+        data_size: A number of samples in the generated dataset.
+        n_num: A number of numerical columns in the generated dataset.
+        n_cat: A number of categorical columns in the generated dataset.
+        n_rounds: A number of randomly generated datasets to test the formats.
+        as_category: If True, then categorical columns will be converted into
+            pandas.Category type before saving.
+
+    """
+    runs = []
+
+    for i in range(n_rounds):
+        print(f'Benchmarking round #{i + 1:d}')
+        print('\tgenerating dataset...')
+        dataset, _ = generate_dataset(data_size, n_num, n_cat)
+
+        if as_category:
+            print('\tconverting categorical columns into pandas.Category')
+            cat_cols = dataset.select_dtypes(include=object).columns
+            dataset[cat_cols] = dataset[cat_cols].fillna('none').astype('category')
+
+        benchmark = []
+
+        for case in list_of_formats:
+            fmt, params = case if len(case) == 2 else (case[0], {})
+
+            with GC():
+                print('\ttesting format:', fmt)
+                filename = f'random.{fmt}'
+                save, load = get_save_load(dataset, fmt)
+                results = defaultdict(int)
+                results['format'] = fmt
+                results['filename'] = filename
+
+                with MemoryTracker() as tracker:
+                    with Timer() as timer:
+                        save(filename, **params)
+                results['size_mb'] = size_of(filename)
+                results['save_ram_delta_mb'] = tracker.memory / (1024 ** 2)
+                results['save_time'] = float(timer)
+
+                with MemoryTracker() as tracker:
+                    with Timer() as timer:
+                        _ = load(filename)
+                results['load_ram_delta_mb'] = tracker.memory / (1024 ** 2)
+                results['load_time'] = float(timer)
+
+                benchmark.append(results)
+
+            run = pd.DataFrame(benchmark)
+            run['run_no'] = i
+            runs.append(run)
+
+    benchmark = pd.concat(runs, axis=0)
+    benchmark.reset_index(inplace=True, drop=True)
+    return benchmark
